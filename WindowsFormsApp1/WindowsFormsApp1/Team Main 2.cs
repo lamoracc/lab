b7f882431c9ba027b1f748ececf523b505ab2658
lamorac <rodionb1708@gmail.com>
2022-03-31 01:57:03 +0300

    qwerty

---
+++ WindowsFormsApp1/WindowsFormsApp1/Team Main 2.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Team_Main_2 : Form
+    {
+        public Team_Main_2()
+        {
+            InitializeComponent();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Teams_main();
+            adminForm.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Visitor_Menu();
+            adminForm.Show();
+        }
+
+        private void label33_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_detail();
+            adminForm.Show();
+        }
+
+        private void label37_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_detail();
+            adminForm.Show();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_detail();
+            adminForm.Show();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_detail();
+            adminForm.Show();
+        }
+
+        private void label21_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_detail();
+            adminForm.Show();
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_detail();
+            adminForm.Show();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_detail();
+            adminForm.Show();
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_detail();
+            adminForm.Show();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_detail();
+            adminForm.Show();
+        }
+
+        private void label22_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_detail();
+            adminForm.Show();
+        }
+
+        private void label26_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_detail();
+            adminForm.Show();
+        }
+
+        private void label38_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_detail();
+            adminForm.Show();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_detail();
+            adminForm.Show();
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_detail();
+            adminForm.Show();
+        }
+
+        private void label23_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Team_detail();
+            adminForm.Show();
+        }
+
+        private void Team_Main_2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Team detail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Team_detail : Form
+    {
+        public Team_detail()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form adminForm = new Teams_main();
+            adminForm.Show();
+        }
+    }
+}
